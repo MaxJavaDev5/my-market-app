8322428229dee2c69e8ffbfe4d9bf5eaf06d20c8
Max <max2008sh@gmail.com>
2026-04-13 12:15:52 +0200

    add card controller

---
+++ src/main/java/ru/yandex/practicum/market/cart/CartController.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.market.cart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CartController {
+
+	private final CartService cartService;
+
+	public CartController(CartService cartService) {
+		this.cartService = cartService;
+	}
+
+	@GetMapping("/cart/items")
+	public String getCart(Model model) {
+		model.addAttribute("items", cartService.getCartItems());
+		model.addAttribute("total", cartService.getTotalSum());
+		return "cart";
+	}
+
+	@PostMapping("/cart/items")
+	public String updateCart(
+			@RequestParam Long id,
+			@RequestParam String action,
+			Model model
+	) {
+		cartService.updateCart(id, action);
+		model.addAttribute("items", cartService.getCartItems());
+		model.addAttribute("total", cartService.getTotalSum());
+		return "cart";
+	}
+}
