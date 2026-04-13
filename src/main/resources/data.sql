84f371a10d40466438d4c68fc453736beedb23ef
Max <max2008sh@gmail.com>
2026-04-13 10:07:14 +0200

    add default data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO items (title, description, img_path, price) VALUES
+  ('Зонт', 'Зонт синий', '/images/umbrella.jpg', 2000),
+  ('Мяч футбольный', 'Профессиональный мяч', '/images/ball.jpg', 3000),
+  ('Бейсболка', 'Бейсболка красная"', '/images/cap.jpg', 15000);
