c19cc906eb9e0e5c2edceac818ddc64c22b0f4b5
Max <max2008sh@gmail.com>
2026-04-27 23:22:19 +0200

    edit payment client config

--- main-app/src/main/java/ru/yandex/practicum/market/payment/PaymentClientConfig.java
+++ main-app/src/main/java/ru/yandex/practicum/market/payment/PaymentClientConfig.java
@@ -3,6 +3,14 @@ package ru.yandex.practicum.market.payment;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import ru.yandex.practicum.market.payment.client.ApiClient;
 import ru.yandex.practicum.market.payment.client.api.DefaultApi;
 
@@ -10,9 +18,38 @@ import ru.yandex.practicum.market.payment.client.api.DefaultApi;
 @EnableConfigurationProperties(PaymentProperties.class)
 public class PaymentClientConfig {
 
+	private static final String PAYMENT_CLIENT_REGISTRATION_ID = "keycloak";
+
 	@Bean
-	public ApiClient paymentApiClient(PaymentProperties paymentProperties) {
-		return new ApiClient().setBasePath(paymentProperties.getBaseUrl());
+	public ReactiveOAuth2AuthorizedClientManager reactiveOAuth2AuthorizedClientManager(
+			ReactiveClientRegistrationRepository clientRegistrationRepository)
+	{
+		var authorizedClientService =
+				new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistrationRepository);
+
+		var authorizedClientProvider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+				.clientCredentials()
+				.build();
+
+		var authorizedClientManager =
+				new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+						clientRegistrationRepository,
+						authorizedClientService);
+		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+		return authorizedClientManager;
+	}
+
+	@Bean
+	public WebClient paymentWebClient(ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+		ServerOAuth2AuthorizedClientExchangeFilterFunction oauthFilter =
+				new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+		oauthFilter.setDefaultClientRegistrationId(PAYMENT_CLIENT_REGISTRATION_ID);
+		return WebClient.builder().filter(oauthFilter).build();
+	}
+
+	@Bean
+	public ApiClient paymentApiClient(PaymentProperties paymentProperties, WebClient paymentWebClient) {
+		return new ApiClient(paymentWebClient).setBasePath(paymentProperties.getBaseUrl());
 	}
 
 	@Bean
