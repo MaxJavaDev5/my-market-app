adb03a9c599adcb865e0b5e6829a66e0435de6f7
Max <max2008sh@gmail.com>
2026-04-27 23:23:05 +0200

    edit SecurityConfig

--- main-app/src/main/java/ru/yandex/practicum/market/config/SecurityConfig.java
+++ main-app/src/main/java/ru/yandex/practicum/market/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -13,23 +13,21 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig
 {
+
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http)
 	{
-		return http
-				.authorizeExchange(exchange -> exchange
+		return http.authorizeExchange(exchange -> exchange
 						.pathMatchers("/", "/items", "/items/**", "/images/**").permitAll()
 						.pathMatchers("/css/**", "/js/**", "/webjars/**", "/favicon.ico").permitAll()
-						.anyExchange().authenticated()
-				)
+						.anyExchange().authenticated())
 				.formLogin(Customizer.withDefaults())
 				.logout(Customizer.withDefaults())
 				.build();
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder()
-	{
-		return new BCryptPasswordEncoder();
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 }
