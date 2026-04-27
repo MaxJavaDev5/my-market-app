1ec572b21e8be72b24a5765b479b1b02421d63fd
Max <max2008sh@gmail.com>
2026-04-27 19:01:52 +0200

    add SecurityConfig

---
+++ main-app/src/main/java/ru/yandex/practicum/market/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.market.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig
+{
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http)
+	{
+		return http
+				.authorizeExchange(exchange -> exchange
+						.pathMatchers("/", "/items", "/items/**", "/images/**").permitAll()
+						.pathMatchers("/css/**", "/js/**", "/webjars/**", "/favicon.ico").permitAll()
+						.anyExchange().authenticated()
+				)
+				.formLogin(Customizer.withDefaults())
+				.logout(Customizer.withDefaults())
+				.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+}
